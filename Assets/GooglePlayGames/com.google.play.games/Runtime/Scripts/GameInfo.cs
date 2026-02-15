cd47dd2ac0a98d2cce4b3e49ba93c49edef4188e
Syed Ahmed Malik <syedahmedmaliksam@gmail.com>
2026-02-15 16:33:47 +0500

    .

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "1084101000354"; // Filled in automatically
+        public const string ApplicationId = "170855159499"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "1084101000354-ffmm5e4nestuu1cu93cld802ka6gdlbp.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = "170855159499-qte2eekcmqsggeod9bu8hmtorco0p18m.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "com.DefaultCompany.BUDDYCHECK";
 
         public static bool ApplicationIdInitialized() {
