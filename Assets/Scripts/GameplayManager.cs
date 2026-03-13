a147061dfe276506da365066d9b354188695a363
Syed Ahmed Malik <syedahmedmaliksam@gmail.com>
2026-03-13 22:26:38 +0500

    Some Minor Changes to get production ready
    (enabled the intro panel and one small issue in script)
    
    App Bundle 5

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -888,7 +888,7 @@ public class GameplayManager : MonoBehaviourPunCallbacks
             profile.showAnswers();
 
             int difference = Mathf.Abs(guess.Value - currentHostAnswer);
-            profile.Correct(difference <= 2);
+            profile.Correct(difference <= 1);
         }
     }
 
